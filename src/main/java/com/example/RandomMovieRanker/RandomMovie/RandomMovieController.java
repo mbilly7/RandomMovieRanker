5af78a695575157c94fdb43f0070f4e4ecd76304
stewa <stewa@MacBook-Air.fios-router.home>
2022-11-04 21:25:14 -0400

    Add basic Thymeleaf example

--- src/main/java/com/example/RandomMovieRanker/RandomMovie/RandomMovieController.java
+++ src/main/java/com/example/RandomMovieRanker/RandomMovie/RandomMovieController.java
@@ -1,15 +1,22 @@
 package com.example.RandomMovieRanker.RandomMovie;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class RandomMovieController {
     @Autowired
     RandomMovieService randomMovieService;
 
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Billy") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
     @RequestMapping("/randomMovie")
     public RandomMovie getRandomMovie() {
         return randomMovieService.getRandomMovie();
